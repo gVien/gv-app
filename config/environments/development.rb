ceab8a4ed43d985a1219aee1a86fefc2b644f237
gVien <wavetech86@gmail.com>
2016-01-07 13:26:28 -0800

    install devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # set up mailer preview for the account activation & password reset etc
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
