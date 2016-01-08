8fb6bef413e13da8c2ee10845e7682b261d3f309
gVien <wavetech86@gmail.com>
2016-01-07 18:22:42 -0800

    add user model test coverage

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Devise already has these tests for users covered, but redoing it!
+
+  describe "validity of the users model" do
+    it "has a valid user factory" do
+      expect(build(:user)).to be_valid
+    end
+
+    it "is invalid without an email" do
+      expect(build(:user, email: nil)).to_not be_valid
+    end
+
+    it "is invalid without a password" do
+      expect(build(:user, password: nil)).to_not be_valid
+    end
+  end
 end
