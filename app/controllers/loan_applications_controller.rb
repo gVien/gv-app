9c70a1c7be4f9ec53f22df8407b75e2da5cf9f18
gVien <wavetech86@gmail.com>
2016-01-09 10:49:23 -0800

    make ajax call to modal form to create loan application

--- app/controllers/loan_applications_controller.rb
+++ app/controllers/loan_applications_controller.rb
@@ -10,5 +10,22 @@ class LoanApplicationsController < ApplicationController
   end
 
   def create
+    @loan_app = current_user.loan_applications.create(loan_params)  # ajax will take care of displaying the errors
+    @loan_apps = current_user.loan_applications   # for ajax to render all loan apps
+
+    # this works without Ajax but combining the two creates incompatibility (will fix this sometime later)
+    # remember to set `config.action_view.embed_authenticity_token_in_remote_forms = true` in the application.rb in config folder to allow people with no JS (e.g. noscript in Firefox) to use form
+    #
+    # if @loan_app.save
+    #   # @loan_apps = current_user.loan_applications
+    #   redirect_to root_path
+    # else
+    #   render "new"
+    # end
   end
+
+  private
+    def loan_params
+      params.require(:loan_application).permit(:amount, :down_payment, :interest)
+    end
 end
