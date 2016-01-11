e09c1ed8bfbc1e10e850e00cf7c91f38d9a9085a
gVien <wavetech86@gmail.com>
2016-01-10 23:49:04 -0800

    prevent PDF generation error when @loan_app is not valid

--- app/controllers/loan_applications_controller.rb
+++ app/controllers/loan_applications_controller.rb
@@ -29,8 +29,11 @@ class LoanApplicationsController < ApplicationController
   def create
     @loan_app = current_user.loan_applications.create(loan_params)  # ajax will take care of displaying the errors
     @loan_apps = current_user.loan_applications   # for ajax to render all loan apps
-    pdf = LoanApplicationPdf.new(@loan_app, view_context)  # add view_context to use Rails view helpers in pdf class
-    @loan_app.generate(pdf)
+
+    if @loan_app.valid? # if @loan_app is not valid, it won't create the PDF or create the database
+      pdf = LoanApplicationPdf.new(@loan_app, view_context)  # add view_context to use Rails view helpers in pdf class
+      @loan_app.generate(pdf)
+    end
 
     # this works without Ajax but combining the two creates incompatibility (will fix this sometime later)
     # remember to set `config.action_view.embed_authenticity_token_in_remote_forms = true` in the application.rb in config folder to allow people with no JS (e.g. noscript in Firefox) to use form
