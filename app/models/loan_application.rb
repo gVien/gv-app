8dbf5cc2a054bac49b04ce47b716b2bfa0f42e4f
gVien <wavetech86@gmail.com>
2016-01-10 20:53:37 -0800

    set up paperclip attachment and connect w/ aws s3

--- app/models/loan_application.rb
+++ app/models/loan_application.rb
@@ -1,5 +1,7 @@
 class LoanApplication < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :document
+  validates_attachment :document, content_type: { content_type: "application/pdf" }
 
   validates :amount, :down_payment, :interest, presence: true, numericality: { greater_than: 0 }
 end
