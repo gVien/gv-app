45d989e00b40dcf692555fa0344fbaa52ef77776
gVien <wavetech86@gmail.com>
2016-01-11 18:35:30 -0800

    refactor session test & add user sign in with invalid email/pass test

--- spec/features/user_sessions_spec.rb
+++ spec/features/user_sessions_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 RSpec.feature "User Sessions", type: :feature do
-  before { FactoryGirl.create(:user) }
-
-  scenario 'with valid credentials and then sign out' do
-    visit new_user_session_path
-    fill_in 'Email', with: "gai@gai.com"
-    fill_in 'Password', with: "12345678"
-    # click_on 'Log In'   # apparently, there are 2 "Log In" in main page which confuses Capybara (!!!)
-    page.find('.btn.btn-primary').click
+  scenario 'user sign in with VALID credentials and then signout' do
+    # see support/session_helpers for detail of this method
+    sign_in_default_user
 
     expect(page).to have_content('Signed in successfully.')
+
+    click_link "Log out"
+    expect(page).to have_content("Signed out successfully.")
+  end
+
+  scenario 'user sign in with INVALID credentials' do
+    # see support/session_helpers for detail of this method
+    sign_in_with("invalid@user.com", "invalidpass")
+
+    expect(page).to have_content('Invalid email or password.')
   end
 end
