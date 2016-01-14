1590c9e643bab57bcc604cc9d86a2ec266993aa2
gVien <wavetech86@gmail.com>
2016-01-13 22:45:29 -0800

    improve show doc id method & upcase the doc_id

--- app/helpers/loan_applications_helper.rb
+++ app/helpers/loan_applications_helper.rb
@@ -1,5 +1,7 @@
 module LoanApplicationsHelper
+  # shows loan_app id when legacy user does not have doc_id
+  # otherwise shows doc_id (upcase) if it exists
   def show_doc_id(loan_app)
-    loan_app.doc_id || loan_app.id
+    loan_app.doc_id.nil? ? loan_app.id : loan_app.doc_id.upcase
   end
 end
